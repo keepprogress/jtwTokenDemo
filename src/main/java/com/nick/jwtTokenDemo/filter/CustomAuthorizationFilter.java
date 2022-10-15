69d5d050a8bc61d06ec81990923f09fccd1ae0a2
keepprogress <keepprogress@gmail.com>
2022-10-15 12:54:08 +0800

    Build a refresh token controller

--- src/main/java/com/nick/jwtTokenDemo/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/nick/jwtTokenDemo/filter/CustomAuthorizationFilter.java
@@ -29,7 +29,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(request.getServletPath().equals("/api/login")) {
+        if(request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh")) {
             /**
              * do nothing just pass the request to the next filter chain
              */
